788c6301b93cfa2e735ddcd8f8576b8d1537aca7
Eda Işıl İnal <edainalisil@gmail.com>
2024-12-14 11:57:42 +0300

    add explore

---
+++ booking/src/app/components/explore.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+import exploreData from "../../../public/data/datas.json"
+
+function Explore() {
+    return (
+        <section className='font-bitter '>
+
+            <h2 className='text-lg sm:text-2xl text-center  mb-6 lg:mb-12 '>Explore Our Hotel Services</h2>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 px-4 gap-4  '>
+                {exploreData.map((service) => (
+                    <article
+                        key={service.id}
+                        className='bg-white w-full   h-28 border rounded-md shadow-sm flex flex-col items-center justify-around'
+                    >
+                        <div>
+                            <Image src={service.imageSrc} width={50} height={50} alt={service.alt} />
+                        </div>
+                        <h3 className='text:md sm:text-lg'>{service.title}</h3>
+                    </article>
+                ))}
+
+            </div>
+        </section>
+    )
+}
+
+export default Explore
